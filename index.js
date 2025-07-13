270c7a9c176f44e17ec478751e0390701a0da35c
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2025-07-13 23:48:02 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import ejs from 'ejs';
 import  { getStates, getLgas } from 'nigeria-states-lga-select';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const port = process.env.PORT || 2000;
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -23,3 +32,5 @@ app.get('/reservation', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
 })
+
+export default app;
